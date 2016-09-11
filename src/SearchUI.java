331a3e8ddf33253e7e92f730739b0e838e9c320c
Torasian <williamhawken@hotmail.co.uk>
2016-09-11 18:19:09 +0800

    loading all the images now loads the whole data set of images

--- src/SearchUI.java
+++ src/SearchUI.java
@@ -27,262 +27,292 @@ import javax.swing.JToggleButton;
 import javax.swing.SpringLayout;
 
 public class SearchUI extends JFrame{
-	
-	// File representing the folder that you select using a FileChooser
-		static File dir;
-		private String currentDirectory = System.getProperty("user.dir");
-		  // array of supported extensions (use a List if you prefer)
-	    static final String[] EXTENSIONS = new String[]{
-	        "jpg","gif", "png", "bmp" // and other formats you need
-	    };
-	    
-	    static FilenameFilter IMAGE_FILTER;
-	    
-	    
-	    private JFrame myFrameJF;
-	    private JScrollPane scrollJSP;
-	    private JButton browseJB, loadAllJB;
-	    private JLabel browseJL;
-	    private JPanel optionJP, imagesJP;
-	    private JToggleButton histoJTB, deepJTB, bothJTB;
-	    private ArrayList<Image> loadedImages = new ArrayList<>();
-	    private String imagePath;
-	    private Image browseImg;
-	    private ImageDatabase imageDB;
-	    private int imageWidth = 300;
-	    
-	    
-	    public SearchUI(){
-	        ImageBean.extractText();
-	    	
-	    	browseJB = new JButton("Browse:");
-	    	browseJL = new JLabel("");
-	    	
-	    	optionJP = new JPanel(new FlowLayout());
-	    	
-	    	histoJTB = new JToggleButton("Colour Histogram");
-	    	deepJTB = new JToggleButton("Deep Learning");
-	    	bothJTB = new JToggleButton("Both");
-	    	loadAllJB = new JButton("Load All");
-	    	
-	    	
-	    	imagesJP = new JPanel(new WrapLayout());
-	    	scrollJSP = new JScrollPane(imagesJP);
-	    	
-	    	initialise();
-	    }
 
-		private void initialise() {
-			
-			getContentPane().setPreferredSize(new Dimension(1000, 1000));;
-			this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-			this.setResizable(true);
-			
-			getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-			setTitle("Search UI");
-			
-			getContentPane().add(browseJB);
-			browseJB.setAlignmentX(CENTER_ALIGNMENT);
-			getContentPane().add(browseJL);
-			
-			getContentPane().add(optionJP);
-			optionJP.add(histoJTB);
-			optionJP.add(deepJTB);
-			optionJP.add(bothJTB);
-			optionJP.add(loadAllJB);
-			
-			
-//			imagesJP.setPreferredSize(new Dimension(400, 400));
-//			scrollJSP.setPreferredSize(new Dimension(600, 600));
-//			scrollJSP.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-			
-			getContentPane().add(scrollJSP);
-			scrollJSP.setVisible(false);
-			
-			loadAllAction();
-			browseButtonAction();
-			bothJTBAction();
-			deepLearningJTBAction();
-			histoColourJTBAction();
-			
-			this.pack();
-			this.setVisible(true);
-			
-		}
-		
-		private void histoColourJTBAction() {
-			histoJTB.addItemListener(new ItemListener() {
-				
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					int state = e.getStateChange();
-					if(state== ItemEvent.SELECTED){
-						imageDB.setExtractColor(true);
-						System.out.println("Color Histogram is Selected");
-					}
-					else{
-						imageDB.setExtractColor(false);
-						System.out.println("Color Histogram is Not selected");
-					}
+	// File representing the folder that you select using a FileChooser
+	static File dir;
+	private String currentDirectory = System.getProperty("user.dir");
+	// array of supported extensions (use a List if you prefer)
+	static final String[] EXTENSIONS = new String[]{
+			"jpg","gif", "png", "bmp" // and other formats you need
+	};
+
+	static FilenameFilter IMAGE_FILTER;
+
+	private JFrame myFrameJF;
+	private JScrollPane scrollJSP;
+	private JButton browseJB, loadAllJB;
+	private JLabel browseJL;
+	private JPanel optionJP, imagesJP;
+	private JToggleButton histoJTB, deepJTB, bothJTB;
+	private ArrayList<Image> loadedImages = new ArrayList<>();
+	private String imagePath;
+	private Image browseImg, img;
+	private ImageDatabase imageDB;
+	private int imageWidth = 300;
+	private ImageBean images;
+	private  ArrayList<String> imagePaths = new ArrayList<>();
+	private int spc_count=-1;
+
+
+	public SearchUI(){
+
+		browseJB = new JButton("Browse:");
+		browseJL = new JLabel("");
+
+		optionJP = new JPanel(new FlowLayout());
+
+		histoJTB = new JToggleButton("Colour Histogram");
+		deepJTB = new JToggleButton("Deep Learning");
+		bothJTB = new JToggleButton("Both");
+		loadAllJB = new JButton("Load All");
+
+
+		imagesJP = new JPanel(new WrapLayout());
+		scrollJSP = new JScrollPane(imagesJP);
+
+		initialise();
+	}
+
+	private void initialise() {
+
+		getContentPane().setPreferredSize(new Dimension(1000, 1000));;
+		this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+		this.setResizable(true);
+
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+		setTitle("Search UI Test");
+
+		getContentPane().add(browseJB);
+		browseJB.setAlignmentX(CENTER_ALIGNMENT);
+		getContentPane().add(browseJL);
+		browseJL.setAlignmentX(CENTER_ALIGNMENT);
+
+		getContentPane().add(optionJP);
+		optionJP.add(histoJTB);
+		optionJP.add(deepJTB);
+		optionJP.add(bothJTB);
+		optionJP.add(loadAllJB);
+
+		getContentPane().add(scrollJSP);
+		scrollJSP.setVisible(false);
+
+		loadAllAction();
+		browseButtonAction();
+		bothJTBAction();
+		deepLearningJTBAction();
+		histoColourJTBAction();
+
+		this.pack();
+		this.setVisible(true);
+
+	}
+
+	private void histoColourJTBAction() {
+		histoJTB.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				int state = e.getStateChange();
+				if(state== ItemEvent.SELECTED){
+					imageDB.setExtractColor(true);
 				}
-			});
-		}
-		
-		private void bothJTBAction() {
-			bothJTB.addItemListener(new ItemListener() {
-				
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					int state = e.getStateChange();
-					if(state== ItemEvent.SELECTED){
-						histoJTB.setEnabled(false);
-						deepJTB.setEnabled(false);
-						imageDB.setExtractFeature(true);
-						imageDB.setExtractColor(true);
-						System.out.println("both features are Selected");
-					}
-					else{
-						histoJTB.setEnabled(true);
-						deepJTB.setEnabled(true);
-						imageDB.setExtractFeature(false);
-						imageDB.setExtractColor(false);
-						System.out.println("both features have been deselected");
-					}
-					
+				else{
+					imageDB.setExtractColor(false);
+					System.out.println("Color Histogram is Not selected");
 				}
-			});
-		}
-		
-		private void deepLearningJTBAction() {
-			deepJTB.addItemListener(new ItemListener() {
-				
-				@Override
-				public void itemStateChanged(ItemEvent e) {
-					int state = e.getStateChange();
-					if(state== ItemEvent.SELECTED){
-						imageDB.setExtractFeature(true);
-						System.out.println("Deep learning is Selected");
-					}
-					else{
-						imageDB.setExtractFeature(false);
-						System.out.println("Deep learning is not selected");
-					}
-					
+			}
+		});
+	}
+
+	private void bothJTBAction() {
+		bothJTB.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				int state = e.getStateChange();
+				if(state== ItemEvent.SELECTED){
+					histoJTB.setEnabled(false);
+					deepJTB.setEnabled(false);
+					imageDB.setExtractFeature(true);
+					imageDB.setExtractColor(true);
+					System.out.println("both features are Selected");
 				}
-			});
-		}
-		
-		private void browseButtonAction() {
-			//opens and loads an image when the browse button is pressed
-			browseJB.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					loadBrowseImage();
+				else{
+					histoJTB.setEnabled(true);
+					deepJTB.setEnabled(true);
+					imageDB.setExtractFeature(false);
+					imageDB.setExtractColor(false);
+					System.out.println("both features have been deselected");
 				}
-			});
+
+			}
+		});
+	}
+
+	private void deepLearningJTBAction() {
+		deepJTB.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				int state = e.getStateChange();
+				if(state== ItemEvent.SELECTED){
+					imageDB.setExtractFeature(true);
+					System.out.println("Deep learning is Selected");
+				}
+				else{
+					imageDB.setExtractFeature(false);
+					System.out.println("Deep learning is not selected");
+				}
+
+			}
+		});
+	}
+
+	private void browseButtonAction() {
+		//opens and loads an image when the browse button is pressed
+		browseJB.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				loadBrowseImage();
+
+			}
+		});
+	}
+
+	private void loadBrowseImage() {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setDialogTitle("Please select a sample image");
+		String path = "";
+		int returnVal =  fileChooser.showOpenDialog(SearchUI.this);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			imagePath = fileChooser.getSelectedFile().getAbsolutePath();
+			browseImg = null;
+			try {
+				browseImg = ImageIO.read(new File(imagePath));
+
+
+
+				
+				System.out.println(images.getFilePath());
+
+				browseImg = browseImg.getScaledInstance(imageWidth, -1, browseImg.SCALE_DEFAULT);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			browseJL.setIcon(new ImageIcon(browseImg));
+			revalidate();
+			repaint();
 		}
-		
-		private void loadBrowseImage() {
-			JFileChooser fileChooser = new JFileChooser();
-			fileChooser.setDialogTitle("Please select a sample image");
-			String path = "";
-			int returnVal =  fileChooser.showOpenDialog(SearchUI.this);
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				imagePath = fileChooser.getSelectedFile().getAbsolutePath();
-				browseImg = null;
+	}
+
+	private void loadAllAction() {
+		loadAllJB.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
 				try {
-					browseImg = ImageIO.read(new File(imagePath));
-					
-//					
-//					 images = new ImageBean(imagePath, browseImg, imageDB);
-					
-					browseImg = browseImg.getScaledInstance(imageWidth, -1, browseImg.SCALE_DEFAULT);
+					loadAllImages("/Users/WSH/Desktop/test_images");
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				browseJL.setIcon(new ImageIcon(browseImg));
-				revalidate();
-				repaint();
-				
 			}
-		}
-		
-		private void loadAllAction() {
-			loadAllJB.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					loadAllImages("/Users/WSH/Desktop/test_images/tree");
-				}
-			});
-		}
-		
-		private void loadAllImages(String path){
-			
-//			path = currentDirectory;
-			dir = new File(path);
-			IMAGE_FILTER = new FilenameFilter() {
-		
-				@Override
-		        public boolean accept(final File dir, final String name) {
-		            
-		            System.out.println(name);
-		        	for (final String ext : EXTENSIONS) {
-		        		
-		                if (name.endsWith("." + ext)) {
-		                    return (true);
-		                }
-		            }
-		            return (false);
-		        }
-			};
-			
-			 if (dir.isDirectory()) { // make sure it's a directory
-		            for (final File f : dir.listFiles(IMAGE_FILTER)) {
-		                Image img = null;
+		});
+	}
 
-		                try {
-		                    img = ImageIO.read(f);
-		                  
-//		                    if(f.isDirectory()){
-//		                    	loadAllImages("/Users/WSH/Desktop/test_images/");
-//		                    }
-//		                    else{
-		                    	loadedImages.add(img);
-//		                    	images = new ImageBean(dir.getPath(), img, imageDB);
-//		                    }
-		                    
-		                   
-		                    
-		
-		                  
-		                    System.out.println(loadedImages.size());
-		                } catch (final IOException e) {
-		                    // handle errors here
-		                }
-		            }
-		        }
-			 else{
-				 int trim = path.lastIndexOf("/");
-				 loadAllImages(path.substring(0, trim));
-			 }
-			 scrollJSP.setVisible(true);
-			 printPictures();
-		}
-		 private void printPictures() {
-				System.out.println(loadedImages.size());
-				for(int i = 0; i < loadedImages.size(); i++){
-					imagesJP.add(new JLabel(new ImageIcon(loadedImages.get(i))));
-					revalidate();
-					repaint();
+	private void loadAllImages(String path) throws IOException{
+
+
+		dir = new File(path);
+		System.out.println(path);
+
+		spc_count++;
+		String spcs = "";
+		for (int i = 0; i < spc_count; i++)
+			spcs += " ";
+
+
+		if(dir.isFile()){
+			for(final String ext : EXTENSIONS){
+				if(dir.getName().endsWith("."+ ext)){
+					imagePaths.add(dir.getAbsolutePath());
+					Image img = null;
+					try{
+						img = ImageIO.read(dir);
+						loadedImages.add(img);
+					} catch (final IOException e){
+						
+					}
 				}
-				
 			}
-	
-		public static void main(String[] args){
-			SearchUI demo = new SearchUI();
 		}
+		else if (dir.isDirectory()) {
+
+			File[] listOfFiles = dir.listFiles();
+			if(listOfFiles!=null) {
+				for (int i = 0; i < listOfFiles.length; i++)
+					iterateDirectory(listOfFiles[i]);
+			} else {
+				System.out.println(spcs + " [ACCESS DENIED]");
+			}
+		}
+		spc_count--;
+
+		
+		scrollJSP.setVisible(true);
+		printPictures();
+	}
+
+
+	private void iterateDirectory(File aFile){
+
+		spc_count++;
+		String spcs = "";
+		for (int i = 0; i < spc_count; i++)
+			spcs += " ";
+
+
+		if(aFile.isFile()){
+			for(final String ext : EXTENSIONS){
+				if(aFile.getName().endsWith("."+ ext)){
+					imagePaths.add(aFile.getAbsolutePath());
+				}
+			}
+		}
+		else if (aFile.isDirectory()) {
+
+			File[] listOfFiles = aFile.listFiles();
+			if(listOfFiles!=null) {
+				for (int i = 0; i < listOfFiles.length; i++)
+					iterateDirectory(listOfFiles[i]);
+			} else {
+				System.out.println(spcs + " [ACCESS DENIED]");
+			}
+		}
+		spc_count--;
+	}
+
+
+
+
+	private void printPictures() {
+		System.out.println(imagePaths.size());
+		for(int i = 0; i < imagePaths.size(); i++){
+			imagesJP.add(new JLabel(new ImageIcon(imagePaths.get(i))));
+			revalidate();
+			repaint();
+		}
+
+	}
+
+
+
+
+	public static void main(String[] args){
+		SearchUI demo = new SearchUI();
+	
+	}
 
 }
